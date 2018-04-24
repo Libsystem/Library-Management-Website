f4437d3ee480ea9de139022c97e604665d8b841e
Libsystem <37008742+Libsystem@users.noreply.github.com>
2018-04-24 15:17:25 -0400

    Add files via upload

---
+++ loginuser.php
@@ -0,0 +1,36 @@
+<?php
+require "loginuser.inc";
+LogInUser();
+?>
+
+<<!DOCTYPE html>
+<html>
+
+<!-- header -->
+<head>
+	<link href="libraryStyle.css" type="text/css" rel="stylesheet">
+ 
+<title> Library management website </title>
+</head>
+	<div class="body">
+<body>
+
+	<div class="header">
+		<h1>Library Management System</h1>
+	</div>
+<!--Navigation Menu -->
+	<div class="sidenav">
+	<a href="index.html">Home</a>
+	<a href="Browse.html">Browse</a>
+	<a href="about_us.html">About</a>
+	<a href="register.html">Register</a>
+	<a href="add_book_form.php">Add Book</a>
+
+	</div>
+
+<!--Footer-->
+	<div class=\"footer\">
+		<p>Comp305 - Database Application Development. </p>
+	</div>
+</body>
+</html>
